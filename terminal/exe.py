69545bad9c9b869f3cdecc2b5e0328ab7d45f223
LEYEXS <99690174+LEYEXS@users.noreply.github.com>
2024-01-16 14:09:52 +0300

    Delete terminal directory

--- terminal/exe.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name="selected_options",
-    version="0.1",
-    description="Description of your program",
-    executables=[Executable("selected_options.py")],
-)
